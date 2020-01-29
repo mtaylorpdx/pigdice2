44372e01f2f3cf3a6111ef2d66c71f05d08529ba
Matt Taylor and Tiffany Nguyen <matt+tiffany@>
2020-01-29 11:30:36 -0800

    create an array for player1 rolls, reset to 0 on roll of 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,33 @@
 // Business Logic for ROLL button
 
-// function rollDice() {
-//   Math.floor(Math.random() * 6) + 1;
-// }
+function Player1(turnTotal, gameTotal) {
+  this.turnTotal = [];
+  this.gameTotal = [gameTotal];
+}
+
+function rollDice() {
+  var diceRoll = 0;
+  diceRoll = Math.floor(Math.random() * 6) + 1;
+  console.log(diceRoll)
+  if (diceRoll === 1) {
+    player1.turnTotal = [parseInt("0")];
+    console.log("turn over");
+  } else {
+      player1.turnTotal.push(diceRoll);
+    return
+  }
+}
 
 
 
 //User Interface for ROLL
+var player1 = new Player1();
 
 $(document).ready(function() {
+
   $("#roll1").click(function(event) {
-      var diceRoll = Math.floor(Math.random() * 6) + 1;
-    console.log(diceRoll);
+
+    rollDice();
+    console.log(player1.turnTotal)
   });
 });
