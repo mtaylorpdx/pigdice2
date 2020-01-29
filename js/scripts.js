e5624363e02a9e3add69ac4c1636e7ca38f4aa34
Matt Taylor and Tiffany Nguyen <tiffany@>
2020-01-29 15:19:49 -0800

    fix gameTotal

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 // Business Logic for ROLL button
-
-function Player1(turnTotal, gameTotal) {
+function Player1() {
   this.turnTotal = [];
-  this.gameTotal = [gameTotal];
+  this.gameTotal = 0;
 }
 
 function rollDice() {
@@ -10,12 +9,22 @@ function rollDice() {
   diceRoll = Math.floor(Math.random() * 6) + 1;
   console.log(diceRoll)
   if (diceRoll === 1) {
-    player1.turnTotal = [parseInt("0")];
+    player1.turnTotal = [0];
     console.log("turn over");
   } else {
       player1.turnTotal.push(diceRoll);
-    return
+
+    return 
   }
+}
+
+function holdDice() {
+  console.log(player1.turnTotal);
+  player1.turnTotal.forEach(function(roll) {
+    console.log(roll);
+    player1.gameTotal = player1.gameTotal + roll;
+  });
+  player1.turnTotal = [];
 }
 
 
@@ -26,8 +35,14 @@ var player1 = new Player1();
 $(document).ready(function() {
 
   $("#roll1").click(function(event) {
-
+    event.preventDefault();
     rollDice();
-    console.log(player1.turnTotal)
+    console.log(player1.turnTotal);
+  });
+
+  $("#hold1").click(function(event) {
+    event.preventDefault();
+    holdDice();
+    console.log(player1.gameTotal);
   });
 });
