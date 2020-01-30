3891d7952ef9536ea76ad98d0ce2f01b2bc81e0c
Matt Taylor and Tiffany Nguyen <tiffany@>
2020-01-30 13:57:24 -0800

    add random numbers to dice images

--- js/scripts.js
+++ js/scripts.js
@@ -3,43 +3,78 @@
 function Player1() {
   this.turnTotal = [];
   this.gameTotal = 0;
+  this.currentRoll = [];
 }
 
 function Player2() {
   this.turnTotal = [];
   this.gameTotal = 0;
+  this.currentRoll = [];
 }
 
 //ROLL button
-function rollDice1() {
-  var diceRoll = 0;
+function rollDice() {
   diceRoll = Math.floor(Math.random() * 6) + 1;
+  diceRoll2 = Math.floor(Math.random() * 6) + 1;
+  // var diceImages = [dice1, dice2, dice3, dice4, dice5, dice6];
+  // var dice1 = ("img/dice1.jpg");
+  // var dice2 = ("img/dice2.jpg");
+  // var dice3 = ("img/dice3.jpg");
+  // var dice4 = ("img/dice4.jpg");
+  // var dice5 = ("img/dice5.jpg");
+  // var dice6 = ("img/dice6.jpg");
+  var diceImages = new Array("img/dice1.jpg", "img/dice2.jpg", "img/dice3.jpg", "img/dice4.jpg", "img/dice5.jpg", "img/dice6.jpg");
+  var img = diceImages[diceRoll - 1];
+  var img2 = diceImages[diceRoll2 - 1];
+  
+  $("#dices").empty();
+  var imgStr = '<img src="' + img + '" alt = "">';
+  var imgStr2 = '<img src="' + img2 + '" alt = "">';
+  
+  console.log(imgStr);
+  console.log(imgStr2);
+  // document.getElementById("#dices").append(imgStr);
+
+  $("#dices").append(imgStr, imgStr2);
+  // $("#dices").append(imgStr2);
+
   console.log(diceRoll)
-  if (diceRoll === 1 && currentPlayer === 1) {
+  console.log(diceRoll2)
+  if ((diceRoll === 1 || diceRoll2 === 1) && currentPlayer === 1) {
     player1.turnTotal = [0];
     $(".result1").text("Player 1 Busts!");
     $(".result2").empty();
     currentPlayer = 2;
     console.log("turn over");
-  } else if (diceRoll === 1 && currentPlayer === 2) {
+  } else if ((diceRoll === 1 || diceRoll2 === 1) && currentPlayer === 2) {
     player2.turnTotal = [0];
     $(".result2").text("Player 2 Busts!");
     $(".result1").empty();
     currentPlayer = 1;
     console.log("turn over");
   } else if (currentPlayer === 1) {
-    $(".result1").append("<li>" + diceRoll + "</li>");
+    $(".result1").append("<li>" + diceRoll + " " + diceRoll2 + "</li>");
     $(".result2").empty();
-    player1.turnTotal.push(diceRoll);
+    player1.turnTotal.push(diceRoll, diceRoll2);
   } else {
-    player2.turnTotal.push(diceRoll);
+    player2.turnTotal.push(diceRoll, diceRoll2);
     $(".result1").empty();
-    $(".result2").append("<li>" + diceRoll + "</li>");
+    $(".result2").append("<li>" + diceRoll + " " + diceRoll2 + "</li>");
     return 
   }
 }
+
+// rollDice.prototype.displayDice = function(diceRoll, diceRoll2) {
+//   var diceRoll = diceRoll;
+//   var diceRoll2 = diceRoll2;
+//   $("#" + diceRoll).show();
+//   $("#" + diceRoll2).show();
+// }
+
+
+
 //HOLD button
-function holdDice1() {
+function holdDice() {
   console.log(player1.turnTotal);
   if (currentPlayer === 1) {
     player1.turnTotal.forEach(function(roll) {
@@ -74,15 +109,16 @@ $(document).ready(function() {
 
   $("#roll1").click(function(event) {
     event.preventDefault();
-    rollDice1();
-
+    // rollDice.displayDice(diceRoll, diceRoll2);
+    rollDice();
+  
     console.log(player1.turnTotal);
     console.log(player2.turnTotal);
   });
 
   $("#hold1").click(function(event) {
     event.preventDefault();
-    holdDice1();
+    holdDice();
     $("#scoreTotal1").empty().text(player1.gameTotal);
     $("#scoreTotal2").empty().text(player2.gameTotal);
     console.log(player1.gameTotal);
